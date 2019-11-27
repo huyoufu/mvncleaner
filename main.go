3f516e3833d310c15cfa757c478f22e87295dc47
huyoufumac <huyoufu1988@163.com>
2019-11-27 22:12:45 +0800

    添加了无用的error文件夹和unknown无用文件夹删除

--- main.go
+++ main.go
@@ -7,38 +7,39 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
-func main(){
+func main() {
 
 	dir := getRepoDir()
 	clean(dir)
 
 }
-func getRepoDir()string{
-	repoDir:=""
+func getRepoDir() string {
+	repoDir := ""
 	args := os.Args
-	if len(args)>1 {
-		repoDir=args[1]
-	}else{
+	if len(args) > 1 {
+		repoDir = args[1]
+	} else {
 
 		//没有指明路径参数 获取maven的安装目录
 		home := GetMavenHome()
-		if home=="" {
+		if home == "" {
 			fmt.Println("没有找到MAVEN的安装目录.如果不想设置 可以执行命名的时候 传入maven的目录参数(推荐此参数)!!!")
 			os.Exit(0)
 		}
 
-		repoDir=parseConfig4repoDir(home)
+		repoDir = parseConfig4repoDir(home)
 	}
 	return repoDir
 }
 
-func parseConfig4repoDir(home string)string{
-	configFile:=filepath.Join(home,"conf","settings.xml")
+func parseConfig4repoDir(home string) string {
+	configFile := filepath.Join(home, "conf", "settings.xml")
 	//接下xml文件
-	f, err := os.Open(configFile);
+	f, err := os.Open(configFile)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 	}
@@ -55,40 +56,38 @@ func parseConfig4repoDir(home string)string{
 	return v.LocalRepository
 }
 
-
-
 func clean(dir string) {
 
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 
-		//if strings.Contains(path, "unknown"){
-		//	fmt.Println("正在删除文件/文件夹:",path)
-		//	delErr:=os.Remove(path)
-		//	if delErr!=nil {
-		//		fmt.Println(delErr)
-		//	}
-		//}
-		if strings.Contains(path, "lastUpdated"){
-			fmt.Println("正在删除文件/文件夹:",path)
-			delErr:=os.Remove(path)
-			if delErr!=nil {
+		if strings.Contains(path, "unknown") || strings.Contains(path, "/error") {
+			fmt.Println("正在删除文件/文件夹:", path)
+			delErr := os.RemoveAll(path)
+			if delErr != nil {
+				fmt.Println(delErr)
+			}
+		}
+		if b, _ := regexp.MatchString("\\$\\{.*\\}", path); b {
+			fmt.Println("正在删除文件/文件夹:", path)
+			os.RemoveAll(path)
+		}
+
+		if strings.Contains(path, "lastUpdated") {
+			fmt.Println("正在删除文件/文件夹:", path)
+			delErr := os.Remove(path)
+			if delErr != nil {
 				fmt.Println(delErr)
 			}
 		}
 
-		//if b,_:=regexp.MatchString("\\$\\{.*\\}",path);b {
-		//	fmt.Println("正在删除文件/文件夹:",path)
-		//	os.Remove(path)
-		//}
 		return err
 	})
 
-
 }
-func GetMavenHome()string{
+func GetMavenHome() string {
 	home := os.Getenv("M2_HOME")
-	if home=="" {
-		home=os.Getenv("MAVEN_HOME")
+	if home == "" {
+		home = os.Getenv("MAVEN_HOME")
 	}
 	return home
 }
