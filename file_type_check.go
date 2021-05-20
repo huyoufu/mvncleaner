ddc0cec9d26f3a297bce5cd7e11e789dd5105025
huyoufu <huyoufu@163.com>
2021-05-21 03:14:52 +0800

    为了照顾某些在xml文件写 shit code的人

--- file_type_check.go
+++ file_type_check.go
@@ -69,7 +69,8 @@ func checkXmlFileType(path string) bool {
 	if err != nil {
 		return false
 	} else {
-		b := strings.Contains(line, "<?xml") || strings.Contains(line, "<project>")
+		//严格意义上说 一个标准的xml文件 就是应该 <?xml开头 但是有些 shit code 不按照规矩来 上来给你写个注释..
+		b := strings.Contains(line, "<?xml") || strings.Contains(line, "<project>") || strings.Contains(line, "<!--")
 		if !b {
 			//正在检查的文件存在问题
 			fmt.Printf("正在检查的文件%s 不是xml文件 请仔细检查~~~\n", path)
